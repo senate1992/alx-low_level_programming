d5a7bca884b5bf7ab189d062a1b41f0ee4df7ca8
Temitope <olatunbosuntemitope64@gmail.com>
2023-02-16 10:44:21 -0800

    ‘ALX’

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,16 +2,16 @@
 
 
 /**
- *  main - A programs that prints a line using the printf function*
+ *  main - prints "with proper grammer, but the outcome is a piece of art"
  *  Return: Always 0.
+ *
  */
 
 
 int main(void)
 
-
 {
-	printf("with proper grammar, but the outcome is a piece of art,")
+	printf("with proper grammar, but the outcome is a piece of art,\n")
 	return (0);
 	
 }
