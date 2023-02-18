27829d1e3e9a07208071d82ecce3651de59391a7
Temitope <olatunbosuntemitope64@gmail.com>
2023-02-18 05:07:43 -0800

    Update on task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,13 @@ int main(void)
 {
 	int d;
 
-	for (d = '0'; d < 10;)
-	d++;
+	for (d = '0'; d < 10; 
+			d++);
+
 	{
 	putchar(d);
 
-	if (d != '9')
+	if (d < '9')
 	{
 	putchar(',');
 	putchar(' ');
