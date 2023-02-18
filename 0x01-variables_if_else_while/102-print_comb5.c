7a55ce4344ddbdab4e33090392bade884e86c063
Temitope <olatunbosuntemitope64@gmail.com>
2023-02-18 08:22:51 -0800

    Update on task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,14 +10,18 @@
 int main(void)
 
 {
-	int i, j;
-	for (i = 0; i <= 9; i++)
+	int num1, num2;
+    
+	for (num1 = 0; num1 <= 99; num1++)
 	{
-	putchar(i + '0');
-	for (j = 0; j <= 9; j++) 
+	for (num2 = num1 + 1; num2 <= 99; num2++)
 	{
-	putchar(j + '0');
-	if (i != 9 || j != 9) 
+	putchar((num1 / 10) + '0');
+	putchar((num1 % 10) + '0');
+	putchar(' ');
+	putchar((num2 / 10) + '0');
+	putchar((num2 % 10) + '0');
+	if (num1 != 98 || num2 != 99)
 	{
 	putchar(',');
 	putchar(' ');
