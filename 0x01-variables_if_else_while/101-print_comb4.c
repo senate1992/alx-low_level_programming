094346f5e614889ff67aae90cc14eae1b73fa1fb
Temitope <olatunbosuntemitope64@gmail.com>
2023-02-18 06:56:39 -0800

    Update on task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,18 +13,19 @@ int main(void)
 {
 
 	int d, p, q;
-			for (d = '0'; d < '9'; d++);
+			for (d = '0'; d < '9'; 
+					d++);
 
 	{
-	
+
 	for (p = d + 1; p <= '9'; p++)
-	
-	
+
+
 	{
 
 	for (q = p + 1; q <= '9'; q++)
-	
-	
+
+
 	{
 	if ((p != d) != q)
 	{
