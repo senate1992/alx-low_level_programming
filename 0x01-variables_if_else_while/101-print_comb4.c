10060210891d5a20f05c03368d8d18e54aa2d8bc
Temitope <olatunbosuntemitope64@gmail.com>
2023-02-18 08:38:20 -0800

    Update on task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,19 +14,19 @@ int main(void)
 	int i, j, k;
 
 	for (i = 0; i < 10; i++)
-	{
+	
 	for (j = i + 1; j < 10; j++)
 	{
 	for (k = j + 1; k < 10; k++)
-	
+	{
 	putchar('0' + i);
 	putchar('0' + j);
 	putchar('0' + k);
 	putchar(',');
 	putchar(' ');
+	}
+	}
 	
-	}
-	}
 	putchar('\n');
 	return (0);
 }
