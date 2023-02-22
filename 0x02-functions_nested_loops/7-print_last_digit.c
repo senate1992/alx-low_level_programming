d9ec4a14fd99196b199e8d6e96d7349eb6d587b2
Temitope <olatunbosuntemitope64@gmail.com>
2023-02-22 01:00:03 -0800

    Update on task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,14 +4,15 @@
  * Print_last_digit - Print the last digit of a number
  * @n: The number to be treated
  * Return: the value of the last digit of a number
- */
+ *
+ */
 
 int print_last_digit(int n)
 {
 	int last;
 
 	last = n % 10;
-	if (last <0)
+	if (last < 0)
 	{
 	last = last * -1;
 	}
