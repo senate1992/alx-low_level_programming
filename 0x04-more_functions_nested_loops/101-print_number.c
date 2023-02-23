b69114f0d34cf5b0623ebd9c9f620be0a41574ec
Temitope <olatunbosuntemitope64@gmail.com>
2023-02-23 10:59:20 -0800

    101-print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,17 +7,18 @@
  */
 
 void print_number(int n)
+
 {
-	unsigned int k = n;
+	unsigned int nb;
 
 	if (n < 0)
 	{
-	n *= -1;
-	k = n;
 	_putchar('-');
+	nb = -n;
 	}
-	k /= 10;
-	if (k != 10)
-	print_number(k);
-	_putchar((unsigned int) n % 10 + '0');
+	else
+	nb = n;
+	if (nb / 10)
+	print_number(nb / 10);
+	_putchar(nb % 10 + '0');
 }
