3a8670215ba71a6b5ac46d60c8ea28cfead08b3a
Temitope <olatunbosuntemitope64@gmail.com>
2023-02-23 08:44:49 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the fizz buzz program
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	for (int i = 1; i <= 100; i++)
+	{
+	if (i % 3 == 0 && i % 5 == 0)
+	{
+	printf("FizzBuzz ");
+	}
+	else if (i % 3 == 0)
+	{
+	printf("Fizz ");
+	}
+	else if (i % 5 == 0)
+	{
+	printf("Buzz ");
+	}
+	else
+	{
+	printf("%d ", i);
+	}
+	}
+	printf("\n");
+	return (0);
+}
