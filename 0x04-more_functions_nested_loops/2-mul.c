514bf6ce7bb44fff6a8dbb50162c920933c1de4a
Temitope <olatunbosuntemitope64@gmail.com>
2023-02-23 05:53:32 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,12 +3,12 @@ include "main.h"
 /**
  * mul - multiply two integers
  * @a: first integer to multiply
- * @a: second integer to multiply with first integer
+ * @b: second integer to multiply with first integer
  * Return: The reult of multiplication between x param and y param
  */
 
 int mul(int a, int b)
 
 {
-	return (a*b);
+	return (a * b);
 }
