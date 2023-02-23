f116e162d99fea5840506641dbe27560afd5548f
Temitope <olatunbosuntemitope64@gmail.com>
2023-02-23 09:29:42 -0800

    10-print_triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,6 +10,7 @@ void print_triangle(int size)
 {
 	int x;
 	int y;
+	int z;
 
 	if (size <= 0)
 	{
@@ -17,11 +18,15 @@ void print_triangle(int size)
 	}
 	else
 	{
-	for (x = 1; x <= size; x++)
+	for (x = 0; x < size; x++)
 	{
-	for (y = 1; y <= x; y++)
+	for (y = size - x; y > 1; y--)
 	{
-	_putchar('#');
+	_putchar('32');
+	}
+	for (z = 0; z <= x; z++)
+	{
+	_putchar('35');
 	}
 	_putchar('\n');
 	}
