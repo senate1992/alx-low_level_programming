6a069a771afd6ac6890ba14e6319747367e3e699
Temitope <olatunbosuntemitope64@gmail.com>
2023-02-27 11:04:44 -0800

    alx task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+
+/**
+ * _strlen - return the length of a string
+ * @str: the string to get the length of
+ * Return: the length of str
+ */
+
+int _strlen(char *s)
+
+{
+	size_t length = 0;
+
+	while (*str++)
+	length++;
+	return (length);
+}
