841be885e24ef0753d187c6bfe56ee3c6db1c934
Temitope <olatunbosuntemitope64@gmail.com>
2023-02-27 09:29:20 -0800

    alx task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Takes a pointer to an int and updates
+ *
+ * @n: An interger input pointer
+ *
+ * REturn: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
