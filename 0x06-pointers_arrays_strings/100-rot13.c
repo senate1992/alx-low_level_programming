2d9e49f8a373c2f7c17f1832a03a9bc81f7b9d23
Temitope <olatunbosuntemitope64@gmail.com>
2023-03-01 11:24:31 -0800

    alx

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,20 +7,25 @@
  */
 
 char *rot13(char *s)
-
 {
 	int i, j;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+	while (s[i] != '\0')
 	{
-	for (j = 0; j <= 12; j++)
+	j = 0;
+	while (alpha[j] != '\0')
 	{
-	if (s[i] == "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"[j])
+	if (s[i] == alpha[j])
 	{
-	s[i] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"[j];
+	s[i] = rot13[j];
 	break;
 	}
+	j++;
 	}
+	i++;
 	}
 	return (s);
 }
