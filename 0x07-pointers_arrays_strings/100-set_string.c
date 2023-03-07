d6cbbc79e62fecf873ce160186d30f4e751b58d1
Temitope <olatunbosuntemitope64@gmail.com>
2023-03-07 03:47:38 -0800

    ALX

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: double pointer to change
+ * @to: char to change pointer to
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
