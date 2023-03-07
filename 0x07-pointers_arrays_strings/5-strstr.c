c42c0fa8d7a85d357466947a0e69527bc92e6226
Temitope <olatunbosuntemitope64@gmail.com>
2023-03-07 03:25:17 -0800

    ALX

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string to search through
+ * @needle: substring to look for
+ *
+ * Return: pointer to beginning of located substring or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (needle[j] == '\0')
+			return (haystack + i);
+	}
+	return (NULL);
+}
