ed43139e1700ca880e5ebc8912be53fbe2067a60
Temitope <olatunbosuntemitope64@gmail.com>
2023-03-08 06:35:08 -0800

    alx

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - entry point
+ * @n: input
+ * Return: Always 0.
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+	return -1;
+	}
+	else if (n == 0)
+	{
+	return 1;
+	}
+	else 
+	{
+	return (n * factorial(n - 1));
+	}
+}
