0d4dfb1884a5eb658080010e98274948ed3de988
Temitope <olatunbosuntemitope64@gmail.com>
2023-03-14 05:07:08 -0700

    ALX

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - ...
+ * @grid: ...
+ * @height: ...
+ * Return: ...
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+	free(grid[i]);
+	}
+	free(grid);
+}
