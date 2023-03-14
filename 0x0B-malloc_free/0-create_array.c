090b02b2ace104300b99c9503700dc53b0d920bf
Temitope <olatunbosuntemitope64@gmail.com>
2023-03-14 03:53:59 -0700

    ALX

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - create an array
+ * @size: size of array
+ * @c: the char to fil
+ * Return: the array filled
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	if (size == 0)
+		return (NULL);
+
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+	return (s);
+}
