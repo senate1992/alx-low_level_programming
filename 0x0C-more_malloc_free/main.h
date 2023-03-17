8f8d7fbf9341437e5ece87e7cc30fedb3621a860
Temitope <olatunbosuntemitope64@gmail.com>
2023-03-17 00:49:11 -0700

    alx

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,5 +6,12 @@ void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int main(int argc, char *argv[]);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+int get_digit(char c);
+char *iterate_zeroes(char *str);
+char *create_xarray(int size);
+int find_len(char *str);
 
 #endif /* MAIN_H */
