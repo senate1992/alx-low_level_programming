cac1c4deef3d95129116b8277c120a1b54335dbd
Temitope <olatunbosuntemitope64@gmail.com>
2023-03-20 05:59:10 -0700

    alx

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,8 +1,6 @@
+#include <string.h>
 #include <stdlib.h>
 #include "dog.h"
-
-int _strlen(char *s);
-char *_strcpy(char *src);
 
 /**
  * new_dog - ...
@@ -19,8 +17,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name != NULL && owner != NULL)
 	{
-		name_l = _strlen(name) + 1;
-		own_l = _strlen(owner) + 1;
+		name_l = strlen(name) + 1;
+		own_l = strlen(owner) + 1;
 		max_dog = malloc(sizeof(dog_t));
 		if (max_dog == NULL)
 			return (NULL);
@@ -42,8 +40,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 			return (NULL);
 		}
 
-		max_dog->name = _strcpy(max_dog->name, name);
-		max_dog->owner = _strcpy(max_dog->owner, owner);
+		max_dog->name = strcpy(max_dog->name, name);
+		max_dog->owner = strcpy(max_dog->owner, owner);
 		max_dog->age = age;
 	}
 
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,6 @@
-#ifndef DOG_H
-#define DOG_H
-int _putchar (char c);
+#ifndef _HEADER_
+#define _HEADER_
+
 /**
  * struct dog - data structure
  * @name: name of dog
@@ -17,6 +17,7 @@ struct dog
 };
 
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
