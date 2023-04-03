40b391e409e85ea5582fa7c85d5a9673b4b790ee
Temitope <olatunbosuntemitope64@gmail.com>
2023-04-03 13:32:12 -0700

    alx

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -4,13 +4,14 @@
  * free_listint - Frees a listint_t list.
  * @head: A pointer to the head of the listint_t list to be freed.
  */
-void free_listint(listint_t *head) {
+void free_listint(listint_t *head)
+{
 	listint_t *tmp;
 
-	while (head != NULL)
+	while (head)
 	{
-	tmp = head;
-	head = head->next;
-	free(tmp);
+		tmp = head->next;
+		free(head);
+		head = tmp;
 	}
 }
