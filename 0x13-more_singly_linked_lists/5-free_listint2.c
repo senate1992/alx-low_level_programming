f67786324364e79019a9b597508d2362409a0a1b
Temitope <olatunbosuntemitope64@gmail.com>
2023-04-03 13:13:56 -0700

    alx

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,16 +6,15 @@
  */
 void free_listint2(listint_t **head)
 {
-    if (head == NULL) {
-        return;  // Check for NULL pointer to list
-    }
-    
-    listint_t *current = *head;
-    while (current != NULL) {
-        listint_t *next = current->next;  // Save the next pointer before freeing current
-        free(current);
-        current = next;
-    }
-    
-    *head = NULL;  // Update the head pointer to point to NULL
+	listint_t *temp;
+	if (head == NULL)
+	return;
+	while (*head)
+	{
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+	}
+
+	*head = NULL;
 }
