2943c881a19a5de538e74dc1fb0b8a8c95a3b461
Temitope <olatunbosuntemitope64@gmail.com>
2023-04-12 08:05:12 -0700

    alx

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,53 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include "main.h"
+#include <stddef.h>
 
 /**
- * create_file - A function that creates a file
- * @filename: The filename to create
- * @text_content: A NULL terminated string to write to the file
- * Return: 1 on success, -1 if file can not be created, nor written,
- * nor write fails.
+ * _strlen - It counts the length of the string
+ * @str: input string
+ *
+ * Return: the lenght of the input string
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+ * create_file - It creates a file
+ * @filename: It is the name of the file to create
+ * @text_content: It is a NULL terminated string to write to the file
+ *
+ * Return: On success 1, -1 on failure
+ * If the filename is NULL return -1
+ * If text_content is NULL create an empty file
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fdo, fdw, len = 0;
+	int file, wrote;
 
 	if (filename == NULL)
 		return (-1);
-
-	fdo = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (fdo < 0)
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
 		return (-1);
-
-	while (text_content && *(text_content + len))
-		len++;
-
-	fdw = write(fdo, text_content, len);
-	close(fdo);
-	if (fdw < 0)
-		return (-1);
-	return (1);
+	if (text_content != NULL)
+	{
+		wrote = write(file, text_content, _strlen(text_content));
+		if (wrote == -1)
+		{
+			close(file);
+			return (-1);
+		}
+		close(file);
+		return (1);
+	}
+	else
+	{
+		close(file);
+		return (1);
+	}
 }
