887236badfbb4727199a4a3abe923b64002c3c3c
Temitope <olatunbosuntemitope64@gmail.com>
2023-04-12 08:00:00 -0700

    alx

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,8 +8,7 @@
  * create_file - A function that creates a file
  * @filename: The filename to create
  * @text_content: A NULL terminated string to write to the file
- * Return: 1 on success, -1 if file can not be created, nor written,
- * nor write fails.
+ * Return: 1 on success, -1 if file can not be created
  */
 int create_file(const char *filename, char *text_content)
 {
